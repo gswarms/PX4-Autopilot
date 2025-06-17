a4e6eacef75f64ef051e650bab21432092d1fa81
Iftach Naftaly <iftahnaf@gmail.com>
2025-06-17 10:00:56 +0000

    feat: add interception data to the mavlink streams

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1430,6 +1430,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 4.0f);
 		configure_stream_local("VIBRATION", 0.1f);
 		configure_stream_local("WIND_COV", 0.5f);
+		configure_stream_local("INTERCEPTION_DATA", 10.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 1.0f);
@@ -1649,6 +1650,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 20.0f);
 		configure_stream_local("VIBRATION", 2.5f);
 		configure_stream_local("WIND_COV", 10.0f);
+		configure_stream_local("INTERCEPTION_DATA", 10.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 50.0f);
@@ -1681,6 +1683,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 1.0f);
 		configure_stream_local("BATTERY_STATUS", 0.5f);
 		configure_stream_local("UTM_GLOBAL_POSITION", 0.5f);
+		configure_stream_local("INTERCEPTION_DATA", 10.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
