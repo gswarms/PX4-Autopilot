ef9e94d9b753c56913cdbab6b84340606bbe10bf
Iftach Naftaly <iftahnaf@gmail.com>
2025-06-18 08:42:47 +0000

    fix(logger): record custom uorb topics

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -122,6 +122,8 @@ void LoggedTopics::add_default_topics()
 	add_topic("vehicle_gps_position", 500);
 	add_topic("vehicle_land_detected");
 	add_topic("vehicle_local_position", 100);
+	add_topic("radar_detection", 200);
+	add_topic("interception_data", 200);
 	add_topic("vehicle_local_position_setpoint", 100);
 	add_topic("vehicle_magnetometer", 200);
 	add_topic("vehicle_rates_setpoint", 20);
