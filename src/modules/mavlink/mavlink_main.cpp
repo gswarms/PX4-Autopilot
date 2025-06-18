39f12e52e54921ab850269f1e0e895c73f0dd020
Iftach Naftaly <iftahnaf@gmail.com>
2025-06-18 12:30:30 +0000

    fix(interception_stream): reduce to 2 hz

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1430,7 +1430,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 4.0f);
 		configure_stream_local("VIBRATION", 0.1f);
 		configure_stream_local("WIND_COV", 0.5f);
-		configure_stream_local("INTERCEPTION_DATA", 10.0f);
+		configure_stream_local("INTERCEPTION_DATA", 2.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 1.0f);
@@ -1496,7 +1496,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 10.0f);
 		configure_stream_local("VIBRATION", 0.5f);
 		configure_stream_local("WIND_COV", 10.0f);
-		configure_stream_local("INTERCEPTION_DATA", 10.0f);
+		configure_stream_local("INTERCEPTION_DATA", 2.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 10.0f);
@@ -1651,7 +1651,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 20.0f);
 		configure_stream_local("VIBRATION", 2.5f);
 		configure_stream_local("WIND_COV", 10.0f);
-		configure_stream_local("INTERCEPTION_DATA", 10.0f);
+		configure_stream_local("INTERCEPTION_DATA", 2.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 50.0f);
@@ -1684,7 +1684,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 1.0f);
 		configure_stream_local("BATTERY_STATUS", 0.5f);
 		configure_stream_local("UTM_GLOBAL_POSITION", 0.5f);
-		configure_stream_local("INTERCEPTION_DATA", 10.0f);
+		configure_stream_local("INTERCEPTION_DATA", 2.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
