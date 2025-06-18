71f658dba154d331cd976d33dac91d8ebc6fd597
Iftach Naftaly <iftahnaf@gmail.com>
2025-06-18 11:30:25 +0000

    fix(mavlink_stream): add interception to onboard for sitl

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1496,6 +1496,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 10.0f);
 		configure_stream_local("VIBRATION", 0.5f);
 		configure_stream_local("WIND_COV", 10.0f);
+		configure_stream_local("INTERCEPTION_DATA", 10.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 10.0f);
