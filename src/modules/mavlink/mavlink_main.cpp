e22e4216925fabfb2e42bdc6f0fb0c1d9044bdac
Iftach Naftaly <iftahnaf@gmail.com>
2025-08-03 19:48:06 +0000

    fix(mavlink): interception data at 10hz

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1684,7 +1684,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 1.0f);
 		configure_stream_local("BATTERY_STATUS", 0.5f);
 		configure_stream_local("UTM_GLOBAL_POSITION", 0.5f);
-		configure_stream_local("INTERCEPTION_DATA", 2.0f);
+		configure_stream_local("INTERCEPTION_DATA", 10.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
